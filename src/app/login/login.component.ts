f495ad53a208dbc904f14a552fceb90fd40fdeb1
shimanski <shimanski.alex@gmail.com>
2022-01-25 10:21:36 +0200

    display error message: wrong username or password

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,20 +7,28 @@ import {FormBuilder, Validators} from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
   @ViewChild('loginFormHTMLElement', {read : ElementRef, static : true}) loginFormHTMLElement!: ElementRef;
-
-  loginValid = true;
-  loginForm = this.fb.group({
-    username: [null, Validators.required],
-    password: [null, Validators.required],
-  });
+  loginForm: FormGroup;
   loginTitle: string = 'Sign in to your account';
+  errorMessage: string = '';
 
   constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: [null, Validators.required],
+      password: [null, Validators.required],
+    });
   }
 
   ngOnInit(): void {
+    this.errorMessage = (window as { [key: string]: any })["usernameOrPasswordError"] as string;
+  }
+
+  get username(): FormControl {
+    return <FormControl> this.loginForm.get('username');
+  }
+
+  get password(): FormControl {
+    return <FormControl> this.loginForm.get('password');
   }
 
   onSubmit(): void {
